1c54fff6fee6326119a2813e325c6e81514d9b10
Albertraza <arbert.jr@gmail.com>
2019-11-06 14:10:37 -0400

    Add project files.

---
+++ CelyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CelyWeb.Startup))]
+namespace CelyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
