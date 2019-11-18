5e2db32d557bab02120a8cf2cbb4bfbd534e482c
Albertraza <arbert.jr@gmail.com>
2019-11-18 17:12:09 -0400

    Added view prop to view image selected.

--- CelyWeb/App_Start/BundleConfig.cs
+++ CelyWeb/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace CelyWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                "~/Scripts/site.js"));
         }
     }
 }
