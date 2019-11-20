f8cdce9b6c1d6cdd54fe8fd3f12dd16d3934db4a
Albertraza <arbert.jr@gmail.com>
2019-11-20 12:24:01 -0400

    Added Payments Controller and Index View

---
+++ CelyWeb/Controllers/PaymentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CelyWeb.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        // GET: Payments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
